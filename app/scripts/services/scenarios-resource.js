674cc4e70e5d05e516b35747eec23f24db3082dc
Andres Sainz <andres.sainz@globant.com>
2014-06-19 20:28:37 -0300

    fix jshint

--- app/scripts/services/scenarios-resource.js
+++ app/scripts/services/scenarios-resource.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('Services').factory('ScenariosResource', function ($resource) {
-    return $resource('http://localhost:3000/services/:serviceId/scenarios', {serviceId: '@serviceId'})
+    return $resource('http://localhost:3000/services/:serviceId/scenarios', {serviceId: '@serviceId'});
 });
--- app/scripts/services/services-resource.js
+++ app/scripts/services/services-resource.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('Services').factory('ServicesResource', function ($resource) {
-    return $resource('http://localhost:3000/services')
+    return $resource('http://localhost:3000/services');
 });
