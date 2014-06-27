b26c961cd58e488974806614c03b94708d3747cf
Andres Sainz <andres.sainz@globant.com>
2014-06-27 01:43:26 -0300

    add items-list class and style a bit the service list

--- app/scripts/directives/services-list.js
+++ app/scripts/directives/services-list.js
@@ -1,16 +1,41 @@
 'use strict';
 
-angular.module('Directives').directive('servicesList', function () {
+angular.module('Directives').directive('servicesList', function ($location) {
     return {
         restrict: 'E',
         templateUrl: 'scripts/directives/views/services-list.html',
         link: function(){
 
         },
-        controller: function($rootScope, $scope, ServicesResource){
+        controller: function($rootScope, $scope, $route, ServicesResource){
+            var activeService = {};
             $scope.servicesListOrder = 'name';
 
+            $scope.changeActiveService = function(service){
+                activeService.active = false;
+                $scope.createScenarioActive = false;
+
+                service.active = true;
+                activeService = service;
+
+                $location.path('service/'+service.id);
+            };
+
+            $scope.goCreateService = function(){
+                activeService.active = false;
+                $scope.createScenarioActive = true;
+
+                $location.path('create-service');
+            };
+
             ServicesResource.query(function(response){
+                if( $route.current.params.serviceId ){
+                    activeService = _.findWhere(response, {id: $route.current.params.serviceId});
+                    activeService.active = true;
+                }else{
+                    $scope.createScenarioActive = true;
+                }
+
                 $scope.services = response;
             }, function(response){
                 console.log('error', response);
